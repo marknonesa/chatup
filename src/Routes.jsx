11cc1d955a9590e5280ad0b184ebaf20bf728036
Mark <marklesternonesa@gmail.com>
2023-04-25 15:47:50 +0800

    commit

---
+++ src/Routes.jsx
@@ -0,0 +1,19 @@
+import { UserContext  } from "./UserContext";
+import { useContext } from "react";
+import RegisterAndLoginForm from "./RegisterAndLoginForm";
+import Chat from "./Chat";
+
+export default function Routes() {
+    const {username, id} = useContext (UserContext);
+    
+    if (username) {
+        return <Chat />
+    }
+   
+   
+    return ( 
+        <RegisterAndLoginForm/>
+
+    );
+
+}
